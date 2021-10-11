d47581e1bddd84730bf997d5fef4941915f5faca
Brian Cavalier <brian@briancavalier.com>
2021-10-11 10:56:27 -0400

    Remove unneeded memoize function

--- examples/ddd-onion-pets/lib/memoize.ts
+++
@@ -1,9 +0,0 @@
-import { Fx, fx } from '../../../src/fx'
-
-export const memoize = <Y, R>(f: Fx<Y, R>): Fx<Y, R> => {
-  let memo: undefined | { value: R }
-  return fx(function* () {
-    if (!memo) memo = { value: yield* f }
-    return memo.value
-  })
-}
