c9a77ee48ca83f983a1e2c31bbc8c3f916fd852a
Brian Cavalier <brian@briancavalier.com>
2021-10-25 17:22:55 -0400

    Simplify unbounded concurrency handler

--- src/fiber.ts
+++ src/fiber.ts
@@ -1,5 +1,5 @@
-import { Async, Task, async, disposeNone } from './async'
-import { Fx, defer } from './fx'
+import { Async, async } from './async'
+import { Fx } from './fx'
 
 export type Fiber<D, A> = [Fx<D, void>, Promise<A>]
 
@@ -12,13 +12,3 @@ export const join = <D, A>([dispose, promise]: Fiber<D, A>): Async<D, A> =>
     promise.then(resume, resume)
     return dispose
   })
-
-export const runTask = <D, A>(t: Task<D, A>): Fiber<D, A> => {
-  let _dispose = disposeNone as Fx<D, void>
-  return [defer(() => _dispose), new Promise<A>((resolve) => (_dispose = t(resolve)))]
-}
-
-export const flatten = <D1, D2, A>([dispose, promise]: Fiber<D1, Fiber<D2, A>>): Fiber<D1 | D2, A> => {
-  let _dispose: Fx<D1 | D2, void> = dispose
-  return [defer(() => _dispose), promise.then(([d, p]) => ((_dispose = d), p))]
-}
--- src/handle/unboundedConcurrency.ts
+++ src/handle/unboundedConcurrency.ts
@@ -1,29 +1,29 @@
 import { disposeNone } from '../async'
 import { Concurrent } from '../concurrent'
-import { Fiber, dispose, flatten, runTask } from '../fiber'
-import { Fx, defer, fromIO, handler, run } from '../fx'
+import { Fiber } from '../fiber'
+import { Fx, defer, handler, run } from '../fx'
 
 export const withUnboundedConcurrency = <A>(spawn: (f: () => void) => A, kill: (a: A) => void) =>
   handler(function* (effect: Concurrent<unknown, unknown>) {
-    if (effect instanceof Concurrent) return flatten<unknown, unknown, unknown>(spawnTask(spawn, kill, effect.arg))
+    if (effect instanceof Concurrent) return spawnFiber(spawn, kill, effect.arg)
     return yield effect
   })
 
-const spawnTask = <D1, A, B>(
+const spawnFiber = <D, A, B>(
   spawn: (f: () => void) => A,
   kill: (a: A) => void,
-  f: Fx<never, Fiber<D1, B>>
-): Fiber<D1, Fiber<D1, B>> =>
-  runTask((resume) => {
-    let _dispose: Fx<D1, void> = disposeNone
+  f: Fx<never, Fiber<D, B>>
+): Fiber<D, B> => {
+  let spawned: A
+  let _dispose: Fx<D, void> = disposeNone
 
-    const spawned = spawn(() => {
-      const fiber = run(f)
-      _dispose = dispose(fiber)
-      resume(fiber)
+  const p = new Promise<B>((resolve) => {
+    spawned = spawn(() => {
+      const [dispose, promise] = run(f)
+      _dispose = dispose
+      resolve(promise)
     })
-
-    _dispose = fromIO(() => kill(spawned))
-
-    return defer(() => _dispose)
   })
+
+  return [defer(() => (kill(spawned), _dispose)), p]
+}
